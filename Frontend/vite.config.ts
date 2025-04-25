d8dc9985a83d72ace339945e8bcb6b2753944ec3
Kishan Vyas <kishanvyas308@gmail.com>
2025-04-25 12:45:57 +0530

    [Modify] Update ui and Change context of Website #1

--- Frontend/vite.config.ts
+++ Frontend/vite.config.ts
@@ -17,6 +17,7 @@ export default defineConfig({
   },
   server: {
     port: 5174, // Set the development server to run on port 5000
+    allowedHosts: ['test.prikriti.co.in'],
   },
   resolve: {
     alias: {
